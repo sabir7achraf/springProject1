bfca62fe3347d8ae74a04b5e5601650032f629bc
sabir7achraf <146297826+sabir7achraf@users.noreply.github.com>
2024-07-19 21:55:19 +0100

    Page One

--- FirstCRUD/frontend/src/app/app.module.ts
+++ FirstCRUD/frontend/src/app/app.module.ts
@@ -4,14 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminTemplateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButton,
+    MatIconButton,
+    MatIcon,
+    MatMenu,
+    MatMenuTrigger,
+    MatMenuItem
   ],
   providers: [
     provideAnimationsAsync()
